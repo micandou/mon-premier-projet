ff4698cd810ecd24ded79a268ba68b98db6adda1
micandou <michel.andou@email.com>
2025-04-11 23:33:16 +0000

    Ajout du fichier main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+L@M@1tr1$e
+
+git config --global user.name "micandou"
+git config --global user.email "michel.andou@email.com"
+
+git clone https://github.com/micandou/mon-premier-projet
+"""
+
+print("Hello GitHub !")
+
